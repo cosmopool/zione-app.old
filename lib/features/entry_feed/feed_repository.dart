752c08fb3880bc2db2e77461e3555d698d5c0fc2
Kaio Delphino <kaio@tutamail.com>
2021-12-31 00:21:50 -0300

    feature: implemented separated methods to fetch ticket and agenda feed

--- lib/features/entry_feed/feed_repository.dart
+++ lib/features/entry_feed/feed_repository.dart
@@ -4,33 +4,65 @@ import 'package:zione_app/core/constants.dart';
 import 'package:zione_app/repositories/entry_repository.dart';
 import 'package:zione_app/features/entry/entry_model.dart';
 import 'package:zione_app/features/ticket/ticket_model.dart';
-// import 'package:zione_app/features/appointment/appointment_model.dart';
+import 'package:zione_app/features/agenda/agenda_entry_model.dart';
 
 class FeedRepository extends ChangeNotifier {
-  List listOfEntries = [];
+  List listOfAgendaEntries = [];
+  List listOfTickets = [];
   bool isLoading = false;
 
-  List get getFeed {
-    return listOfEntries;
+  List get getAgendaEntryFeed {
+    return listOfAgendaEntries;
   }
 
-  void clearListOfEntries() {
-    listOfEntries = [];
+  void clearListOfAgendaEntries() {
+    listOfAgendaEntries = [];
   }
 
-  void fetchEntries(Endpoint endpoint) async {
-    isLoading = true;
-    // notifyListeners();
+  List get getTicketFeed {
+    return listOfTickets;
+  }
 
-    clearListOfEntries();
+  void clearListOfTickets() {
+    listOfTickets = [];
+  }
+
+  void fetchAgendaEntries() async {
+    // if there is cached entries dont flag as loading
+    // so screens can use cached list instead of loading screen
+    (listOfTickets != []) ? isLoading = false : isLoading = true;
+
+    List _temp = [];
     final EntryRepository repository = EntryRepository();
-    await repository.fetch(endpoint).then((res) {
+
+    await repository.fetch(Endpoint.agenda).then((res) {
       var temp = res['Result'];
       temp.forEach((e) {
-        listOfEntries.add(Ticket(e));
+        _temp.add(AgendaEntry(e));
       });
     });
 
+    listOfTickets = _temp;
+    isLoading = false;
+    notifyListeners();
+  }
+
+  void fetchTickets() async {
+    // if there is cached entries dont flag as loading
+    // so screens can use cached list instead of loading screen
+    (listOfTickets != []) ? isLoading = false : isLoading = true;
+
+    List _temp = [];
+    final EntryRepository repository = EntryRepository();
+
+    await repository.fetch(Endpoint.tickets).then((res) {
+      var temp = res['Result'];
+      temp.forEach((e) {
+        _temp.add(Ticket(e));
+      });
+    });
+
+    listOfTickets = _temp;
     isLoading = false;
     notifyListeners();
   }
@@ -53,3 +85,50 @@ class FeedRepository extends ChangeNotifier {
     notifyListeners();
   }
 }
+  // List listOfAgendaEntries = [];
+  // List listOfTickets = [];
+  // bool isLoading = false;
+
+  // List get getAgendaEntryFeed {
+  //   return listOfAgendaEntries;
+  // }
+
+  // void clearListOfAgendaEntries() {
+  //   listOfAgendaEntries = [];
+  // }
+
+  // List get getTicketFeed {
+  //   return listOfTickets;
+  // }
+
+  // void clearListOfTickets() {
+  //   listOfTickets = [];
+  // }
+
+  // void fetchAgendaEntries() {
+  //   _fetchList(listOfAgendaEntries, Endpoint.agenda, AgendaEntry);
+  // }
+
+  // void fetchTickets() {
+  //   _fetchList(listOfTickets, Endpoint.tickets, Ticket.fromRequest);
+  // }
+
+  // void _fetchList(List list, Endpoint endpoint, entry) async {
+  //   // if there is cached entries dont flag as loading
+  //   // so screens can use cached list instead of loading screen
+  //   (listOfTickets != []) ? isLoading = false : isLoading = true;
+
+  //   List _temp = [];
+  //   final EntryRepository repository = EntryRepository();
+
+  //   await repository.fetch(endpoint).then((res) {
+  //     var temp = res['Result'];
+  //     temp.forEach((e) {
+  //       _temp.add(entry(e));
+  //     });
+  //   });
+
+  //   list = _temp;
+  //   isLoading = false;
+  //   notifyListeners();
+  // }
