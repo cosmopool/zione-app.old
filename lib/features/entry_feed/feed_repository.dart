66aafcf2d3ef596644dc2c703a676a226865abf6
Kaio Delphino <kaio@tutamail.com>
2021-12-31 01:48:54 -0300

    refactor: removed code repetition

--- lib/features/entry_feed/feed_repository.dart
+++ lib/features/entry_feed/feed_repository.dart
@@ -9,6 +9,7 @@ import 'package:zione_app/features/agenda/agenda_entry_model.dart';
 class FeedRepository extends ChangeNotifier {
   List listOfAgendaEntries = [];
   List listOfTickets = [];
+  List listOfAppointments = [];
   bool isLoading = false;
 
   List get getAgendaEntryFeed {
@@ -27,42 +28,48 @@ class FeedRepository extends ChangeNotifier {
     listOfTickets = [];
   }
 
-  void fetchAgendaEntries() async {
-    // if there is cached entries dont flag as loading
-    // so screens can use cached list instead of loading screen
-    (listOfTickets != []) ? isLoading = false : isLoading = true;
+  void fetchAgendaEntries() {
+    AgendaEntry agendaEntryClass(val) {
+      return AgendaEntry(val);
+    }
 
-    List _temp = [];
-    final EntryRepository repository = EntryRepository();
-
-    await repository.fetch(Endpoint.agenda).then((res) {
-      var temp = res['Result'];
-      temp.forEach((e) {
-        _temp.add(AgendaEntry(e));
-      });
-    });
-
-    listOfTickets = _temp;
-    isLoading = false;
-    notifyListeners();
+    _fetchList(listOfAgendaEntries, Endpoint.agenda, agendaEntryClass);
   }
 
-  void fetchTickets() async {
+  void fetchTickets() {
+    Ticket ticketClass(val) {
+      return Ticket(val);
+    }
+
+    _fetchList(listOfTickets, Endpoint.tickets, ticketClass);
+  }
+
+  void _fetchList(List list, Endpoint endpoint, entryClass) async {
+    // late List list;
+    
     // if there is cached entries dont flag as loading
     // so screens can use cached list instead of loading screen
-    (listOfTickets != []) ? isLoading = false : isLoading = true;
+    (list != []) ? isLoading = false : isLoading = true;
 
-    List _temp = [];
+    List _list = [];
     final EntryRepository repository = EntryRepository();
 
-    await repository.fetch(Endpoint.tickets).then((res) {
+    await repository.fetch(endpoint).then((res) {
       var temp = res['Result'];
       temp.forEach((e) {
-        _temp.add(Ticket(e));
+        _list.add(entryClass(e));
       });
     });
 
-    listOfTickets = _temp;
+    // TODO: don't know why this line doesn't work!!!!
+    // list = _list;
+    // so a coded this hack below
+    switch (endpoint) {
+      case Endpoint.tickets: { listOfTickets = _list; } break;
+      case Endpoint.agenda: { listOfAgendaEntries = _list; } break;
+      case Endpoint.appointments: { listOfAppointments = _list; } break;
+    }
+
     isLoading = false;
     notifyListeners();
   }
@@ -85,50 +92,3 @@ class FeedRepository extends ChangeNotifier {
     notifyListeners();
   }
 }
-  // List listOfAgendaEntries = [];
-  // List listOfTickets = [];
-  // bool isLoading = false;
-
-  // List get getAgendaEntryFeed {
-  //   return listOfAgendaEntries;
-  // }
-
-  // void clearListOfAgendaEntries() {
-  //   listOfAgendaEntries = [];
-  // }
-
-  // List get getTicketFeed {
-  //   return listOfTickets;
-  // }
-
-  // void clearListOfTickets() {
-  //   listOfTickets = [];
-  // }
-
-  // void fetchAgendaEntries() {
-  //   _fetchList(listOfAgendaEntries, Endpoint.agenda, AgendaEntry);
-  // }
-
-  // void fetchTickets() {
-  //   _fetchList(listOfTickets, Endpoint.tickets, Ticket.fromRequest);
-  // }
-
-  // void _fetchList(List list, Endpoint endpoint, entry) async {
-  //   // if there is cached entries dont flag as loading
-  //   // so screens can use cached list instead of loading screen
-  //   (listOfTickets != []) ? isLoading = false : isLoading = true;
-
-  //   List _temp = [];
-  //   final EntryRepository repository = EntryRepository();
-
-  //   await repository.fetch(endpoint).then((res) {
-  //     var temp = res['Result'];
-  //     temp.forEach((e) {
-  //       _temp.add(entry(e));
-  //     });
-  //   });
-
-  //   list = _temp;
-  //   isLoading = false;
-  //   notifyListeners();
-  // }
