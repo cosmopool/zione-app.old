3af1cc66a38f0c62ec18e1193b1759c27f727c52
Kaio Delphino <kaio@tutamail.com>
2021-12-31 00:19:39 -0300

    refactor: decoupled resposability to parse endpoint from enum

--- lib/repositories/entry_repository.dart
+++ lib/repositories/entry_repository.dart
@@ -48,13 +48,23 @@ class EntryRepository {
   // Future<Map> _postContentToCache() async {}
   // Future<Map> _closeContentFromCache() async {}
   // Future<Map> _deleteContentFromCache() async {}
+  String _parseEndpoint(Endpoint endpoint) {
+    late String endpointStr;
+
+    switch (endpoint) {
+      case Endpoint.tickets: {endpointStr = "tickets";} break;
+      case Endpoint.appointments: {endpointStr = "appointments";} break;
+      case Endpoint.agenda: {endpointStr = "agenda";} break;
+    }
+
+    return endpointStr;
+  }
 
   Future<Map> _fetchContentFromServer(Endpoint endpoint) async {
     // fetch content from rest api
     Map result;
 
-    final _endpoint =
-        (endpoint == Endpoint.tickets) ? "tickets" : "appointments";
+    final _endpoint = _parseEndpoint(endpoint);
     final url = Uri.http("${conf.host}:${conf.port}", "/$_endpoint");
     final response = await http.get(url, headers: _headers);
 
@@ -67,8 +77,7 @@ class EntryRepository {
     // post content to rest api
     Map result;
 
-    final _endpoint =
-        (endpoint == Endpoint.tickets) ? "tickets" : "appointments";
+    final _endpoint = _parseEndpoint(endpoint);
     final url = Uri.http("${conf.host}:${conf.port}", "/$_endpoint");
     final response =
         await http.post(url, headers: _headers, body: jsonEncode(content));
@@ -81,8 +90,7 @@ class EntryRepository {
     // close content from rest api
     Map result;
 
-    final _endpoint =
-        (endpoint == Endpoint.tickets) ? "tickets" : "appointments";
+    final _endpoint = _parseEndpoint(endpoint);
     final id = content['id'];
     final validEndpoint = "$_endpoint/$id/actions/close";
     final url = Uri.http("${conf.host}:${conf.port}", "/$validEndpoint");
@@ -99,8 +107,7 @@ class EntryRepository {
     // delete content from rest api
     Map result;
 
-    final _endpoint =
-        (endpoint == Endpoint.tickets) ? "tickets" : "appointments";
+    final _endpoint = _parseEndpoint(endpoint);
     final id = content['id'];
     final validEndpoint = "$_endpoint/$id";
     final url = Uri.http("${conf.host}:${conf.port}", "/$validEndpoint");
@@ -112,40 +119,3 @@ class EntryRepository {
     return result;
   }
 }
-
-// class TicketRepository extends EntryRepository {
-//   final _endpoint = "tickets";
-
-//   Future<Map> fetch() async {
-//     // if online, fetch from server
-//     final result = await _fetchContentFromServer(_endpoint);
-//     // if offline, fetch from cache
-//     return result;
-//   }
-
-//   Future<Map> post(Ticket ticket) async {
-//     // TODO: if it is not possible to fetch from server, use cache
-//     final result = await _postContentToServer(_endpoint, ticket.toMap());
-//     return result;
-//   }
-//   // Future<Map> close(Ticket ticket) async {}
-//   // Future<Map> delete(Ticket ticket) async {}
-
-// }
-
-// class AgendaRepository extends EntryRepository {
-//   final _endpoint = "agenda";
-
-//   Future<Map> fetch() async {
-//     // if online, fetch from server
-//     final result = await _fetchContentFromServer(_endpoint);
-//     // if offline, fetch from cache
-//     return result;
-//   }
-
-//   Future<Map> post(AgendaEntry agendaEntry) async {
-//     // TODO: if it is not possible to fetch from server, use cache
-//     final result = await _postContentToServer(_endpoint, agendaEntry.toMap());
-//     return result;
-//   }
-// }
